48d5235ac1efec6a388827b1ad0e1df12bf6c02c
Joe Idelson <joeidelson@gmail.com>
2024-04-03 10:18:05 -0500

    added cat avatar

---
+++ src/components/Cat.js
@@ -0,0 +1,11 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+
+// Dynamic import 
+const Cat = () => {
+    return (
+        <Avatar src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dcf50b1c-d14d-4807-a4f8-b691505263a1/d5kkolm-884ae770-e9d4-4b7a-a42c-f18090b0cdfd.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZjUwYjFjLWQxNGQtNDgwNy1hNGY4LWI2OTE1MDUyNjNhMVwvZDVra29sbS04ODRhZTc3MC1lOWQ0LTRiN2EtYTQyYy1mMTgwOTBiMGNkZmQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PkIobW1Ikvao7vPqXaHUi5MmooD0zKeHZRNUjQqZsw4"/>
+    );
+};
+
+export default Cat;
