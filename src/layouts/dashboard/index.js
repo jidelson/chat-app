247f99cbe72f55ede25017825f938b5e4a18f302
Joe Idelson <joeidelson@gmail.com>
2024-04-03 11:43:59 -0500

    added logo to sidebar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -3,6 +3,8 @@ import { useTheme } from "@mui/material/styles";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Logo from '../../assets/Images/logo.ico'
+
 const DashboardLayout = () => {
 
   const theme = useTheme();
@@ -12,7 +14,14 @@ const DashboardLayout = () => {
   return (
     <>
       <Box sx={{backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width: 100}}>
-
+        <Box sx={{
+          backgroundColor: theme.palette.primary.main,
+          height: 64,
+          width: 64,
+          borderRadius: 1.5 //this value is multiplied by 8
+        }}>
+        <img src={Logo} alt="Chat App Logo"/>    
+        </Box>
       </Box>
       <Outlet />
     </>
