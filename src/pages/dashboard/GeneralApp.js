5af55c1f477ed9f4dd5e8d1c3bfe6d61fde92d2f
Joe Idelson <joeidelson@gmail.com>
2024-04-09 15:23:42 -0500

    created conversation comp

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Chats from "./Chats";
+import { Box, Stack } from "@mui/material";
+import Conversation from "../../components/Conversation";
 
 
 
 const GeneralApp = () => {
 
   return (
-    <>
+    <Stack direction={"row"} sx={{width: "100%"}} >
       <Chats />
-    </>
+      <Box sx={{height: "100%", width: "calc(100vw - 420px)", backgroundColor: "#fff"}}>
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
