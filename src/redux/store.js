62552de79730c8633ee34da3877a9e7f79babebf
Joe Idelson <joeidelson@gmail.com>
2024-04-17 09:45:04 -0500

    installed redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,3 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch as useAppDispatch, useSelector as useAppSelector } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
