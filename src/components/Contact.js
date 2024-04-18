c4d4a05bc73f14983513dcd9e1e37bd90b399e60
Joe Idelson <joeidelson@gmail.com>
2024-04-18 08:17:41 -0500

    added contactInfo & X to header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,35 @@
-import { Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Box, IconButton, Stack, Typography } from '@mui/material';
 import React from 'react';
+import { X } from 'phosphor-react';
 
 const Contact = () => {
+
+    const theme = useTheme();
     return (
-        <Box sx={{width: 320}} >
-            
+        <Box sx={{width: 320, height: "100vh"}} >
+            <Stack sx={{height: "100%"}}  >
+                <Box sx={{
+                    boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+                    width: "100%",
+                    backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background
+
+                }}>
+                    <Stack  
+                        sx={{height: "100%", p: 2}} 
+                        direction="row" alignItems={"center"} 
+                        justifyContent="space-between" 
+                        spacing={3}
+                    >
+                        <Typography variant='subtitle2'>Contact Info</Typography>
+                        <IconButton>
+                            <X />
+                        </IconButton>
+
+                    </Stack>
+
+                </Box>
+            </Stack>
         </Box>
     );
 };
