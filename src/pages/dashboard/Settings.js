ab1631ddb43bbb723453473bff2967c851e047ae
Joe Idelson <joeidelson@gmail.com>
2024-04-24 09:13:18 -0500

    added L panel layout on settings

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Box, Stack } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
 
 const Settings = () => {
+
+    const theme = useTheme();
+
     return (
-        <div>
-            settings
-        </div>
+        <>
+        <Stack direction={"row"} sx={{width: "100%"}} >
+            {/* Left Panel */}
+            <Box 
+                sx={{
+                    overflowY: "scroll", 
+                    height: "100vh", 
+                    width: 320, 
+                    backgroundColor: theme.palette.mode === "light" 
+                    ? "#F8FAFF" 
+                    : theme.palette.background 
+                    }}
+            >
+
+            </Box>
+            {/* Right Panel */}
+        </Stack> 
+        </>
     );
 };
 
