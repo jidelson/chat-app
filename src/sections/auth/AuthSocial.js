d636bc07335f5fb15030200e39bbc23e90073e09
Joe Idelson <joeidelson@gmail.com>
2024-06-21 09:25:43 -0500

    styled divider on authSocial

--- src/sections/auth/AuthSocial.js
+++ src/sections/auth/AuthSocial.js
@@ -4,7 +4,11 @@ import React from 'react';
 const AuthSocial = () => {
     return (
         <div>
-            <Divider>
+            <Divider sx={{
+                my: 2.5, typography: "overline",
+                 color: "text.disabled", '&::before, &::after': {
+                    borderTopStyle: "dashed"
+                 }}}>
                 OR
             </Divider>
         </div>
