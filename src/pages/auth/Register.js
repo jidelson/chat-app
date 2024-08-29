bcb49676804db9b42c0be75852e98e94fb82eb7b
Joe Idelson <joeidelson@gmail.com>
2024-08-29 10:28:43 -0500

    added privacy policy placeholder

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,33 +1,42 @@
-import { Link, Stack, Typography } from '@mui/material';
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <>
-        <Stack spacing={2} sx={{mb: 5, position: "relative"}}>
-            <Typography variant='h4' >
-                Get Started With Tawk
-            </Typography>
+  return (
+    <>
+      <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+        <Typography variant="h4">Get Started With Tawk</Typography>
 
-            <Stack  direction={"row"} spacing={0.5}>
-                <Typography variant='body2'>Already have an account?</Typography>
-                <Link component={RouterLink} to="/auth/login" variant='subtitle2'>
-                Sign in
-                </Link>
-            </Stack>
-            {/* Register Form */}
-
-            <Typography component={"div"} sx={{color: "text.secondary", mt: 3, typography: 'caption', textAlign: 'center'}}>
-                {'By signing up, I agree to '}
-                <Link underline='always' color="text.primary">
-                    Terms of service
-                </Link>
-
-            </Typography>
+        <Stack direction={"row"} spacing={0.5}>
+          <Typography variant="body2">Already have an account?</Typography>
+          <Link component={RouterLink} to="/auth/login" variant="subtitle2">
+            Sign in
+          </Link>
         </Stack>
-        </>
-    );
+        {/* Register Form */}
+
+        <Typography
+          component={"div"}
+          sx={{
+            color: "text.secondary",
+            mt: 3,
+            typography: "caption",
+            textAlign: "center",
+          }}
+        >
+          {"By signing up, I agree to "}
+          <Link underline="always" color="text.primary">
+            Terms of service
+          </Link>
+          {" and "}
+          <Link underline="always" color="text.primary">
+            Privacy Policy
+          </Link>
+        </Typography>
+      </Stack>
+    </>
+  );
 };
 
 export default Register;
