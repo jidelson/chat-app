66286f23b96fad1954fb0a3b7749a85acca5351f
Joe Idelson <joeidelson@gmail.com>
2024-08-30 09:27:45 -0500

    added return to sign in link

--- src/pages/auth/ResetPassword.js
+++ src/pages/auth/ResetPassword.js
@@ -1,5 +1,7 @@
-import { Stack, Typography } from "@mui/material";
+import { Link, Stack, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 import React from "react";
+import { CaretLeft } from "phosphor-react";
 
 const ResetPassword = () => {
   return (
@@ -8,12 +10,22 @@ const ResetPassword = () => {
         <Typography variant="h3" paragraph>
           Forgot Password?
         </Typography>
-        <Typography sx={{color: "text.secondary", mb: 5}}>
-            Please enter the email address associated with your account and we will email you a link to 
-            reset your password.
+        <Typography sx={{ color: "text.secondary", mb: 5 }}>
+          Please enter the email address associated with your account and we
+          will email you a link to reset your password.
         </Typography>
 
         {/* Reset Password Form */}
+        <Link
+          component={RouterLink}
+          to="/auth/login"
+          color="inherit"
+          variant="subtitle2"
+          sx={{ mt: 3, mx: "auto", alignItems: "center", display: "inline" }}
+        >
+          <CaretLeft />
+          Return to Sign In
+        </Link>
       </Stack>
     </>
   );
