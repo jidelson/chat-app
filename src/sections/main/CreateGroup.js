5d44fe465f38856075b19ac3eaae7c71b2944b82
Joe Idelson <joeidelson@gmail.com>
2024-09-04 11:12:10 -0500

    created title field 4 form on groups

--- src/sections/main/CreateGroup.js
+++ src/sections/main/CreateGroup.js
@@ -1,10 +1,59 @@
 import React from "react";
-import { Dialog, DialogTitle, Slide } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, Slide, Stack } from "@mui/material";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider from "../../components/hook-form/FormProvider";
+import { RHFTextField } from "../../components/hook-form";
 
 // TODO => Create a reusable component
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+
+const CreateGroupForm = ({}) => {
+  const NewGroupSchema = Yup.object().shape({
+    title: Yup.string().required("Title is required"),
+    members: Yup.array().min(2, "Must have at least 2 members"),
+  });
+
+  const defaultValues = {
+    title: "",
+    members: [],
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(NewGroupSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful, isValid },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try{
+// API Call
+console.log("DATA", data)
+    }catch(error){
+        console.log("error", error);
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+            <RHFTextField name="title" label="Title" />
+        </Stack>
+
+    </FormProvider>
+  )
+};
 
 const CreateGroup = ({ open, handleClose }) => {
   return (
@@ -20,6 +69,10 @@ const CreateGroup = ({ open, handleClose }) => {
         {/* Title */}
         <DialogTitle>Create New Group</DialogTitle>
         {/* Content */}
+        <DialogContent>{/* Form */}
+
+            <CreateGroupForm />
+        </DialogContent>
       </Dialog>
     </>
   );
