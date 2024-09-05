4e3c5285b75e92039cd4b8aaf03edeb4d71b6c3d
Joe Idelson <joeidelson@gmail.com>
2024-09-05 10:19:23 -0500

    layout for calls page

--- src/pages/dashboard/Call.js
+++ src/pages/dashboard/Call.js
@@ -1,10 +1,80 @@
+import { Box, Divider, IconButton, Stack, Typography, Link } from '@mui/material';
 import React from 'react';
+import { Search, SearchIconWrapper, StyledInputBase } from '../../components/Search';
+import { MagnifyingGlass, Plus } from 'phosphor-react';
+import { SimpleBarStyle } from '../../components/Scrollbar';
+import { useTheme } from "@mui/material/styles";
 
 const Call = () => {
+    const theme = useTheme();
     return (
-        <div>
-            call
-        </div>
+        <>
+        <Stack direction={"row"} sx={{ width: "100%" }}>
+        {/* Left */}
+        <Box
+          sx={{
+            height: "100vh",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+            width: 320,
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
+            <Stack>
+              <Typography variant="h5">Call Logs</Typography>
+            </Stack>
+
+            <Stack sx={{ width: "100%" }}>
+              <Search>
+                <SearchIconWrapper>
+                  <MagnifyingGlass color="#709CE6" />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search..."
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            </Stack>
+            <Stack
+              direction={"row"}
+              justifyContent="space-between"
+              alignItems={"center"}
+            >
+              <Typography variant="subtitle2" component={Link}>
+                Start New Conversation
+              </Typography>
+              <IconButton onClick={() => {
+              //  setOpenDialog(true);
+              }}>
+                <Plus style={{ color: theme.palette.primary.main }} />
+              </IconButton>
+            </Stack>
+            <Divider />
+            <Stack
+              spacing={3}
+              sx={{ flexGrow: 1, overflow: "scroll", height: "100%" }}
+            >
+              <SimpleBarStyle timeout={500} clickOnTrack={false}>
+                <Stack spacing={2.5}>
+                  {/* */}
+                  <Typography variant="subtitle2" sx={{ color: "#676667" }}>
+                    Pinned
+                  </Typography>
+                  {/** Chat List */}
+                  
+                </Stack>
+              </SimpleBarStyle>
+            </Stack>
+          </Stack>
+        </Box>
+
+        {/* Right */}
+        {/* TODO => Reuse Conversation Component */}
+      </Stack>
+        </>
     );
 };
 
