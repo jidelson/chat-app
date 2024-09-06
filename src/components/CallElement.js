661e55f77c7b3fe7ce1f8b757c82ba998dea2faa
Joe Idelson <joeidelson@gmail.com>
2024-09-06 08:02:00 -0500

    added color condition on arrow

--- src/components/CallElement.js
+++ src/components/CallElement.js
@@ -2,8 +2,9 @@ import { faker } from "@faker-js/faker";
 import { Avatar, Box, Stack, Typography } from "@mui/material";
 import React from "react";
 import StyledBadge from "./StyledBadge";
+import { ArrowDownLeft, ArrowUpRight } from "phosphor-react";
 
-const CallLogElement = ({online}) => {
+const CallLogElement = ({ online, incoming, missed }) => {
   return (
     <>
       <Box
@@ -18,31 +19,41 @@ const CallLogElement = ({online}) => {
         p={2}
       >
         <Stack
-        direction="row"
-        alignItems={"center"}
-        justifyContent="space-between"
-      >
-        {online ? (
-            <StyledBadge
-              overlap="circular"
-              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-              variant="dot"
-            >
-              <Avatar src={faker.image.avatar} />
-            </StyledBadge>
-          ) : (
-            <Avatar src={faker.image.avatar()} alt={faker.name.fullName()} />
-          )}
+          direction="row"
+          alignItems={"center"}
+          justifyContent="space-between"
+        >
+          <Stack spacing={2} direction="row" alignItems={"center"}>
+            {online ? (
+              <StyledBadge
+                overlap="circular"
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                variant="dot"
+              >
+                <Avatar src={faker.image.avatar} />
+              </StyledBadge>
+            ) : (
+              <Avatar src={faker.image.avatar()} alt={faker.name.fullName()} />
+            )}
 
-<Stack spacing={0.3}>
-            <Typography variant="subtitle2">{faker.name.fullName()}</Typography>
-            {/* <Typography variant="caption">{msg}</Typography> */}
+            <Stack spacing={0.3}>
+              <Typography variant="subtitle2">
+                {faker.name.fullName()}
+              </Typography>
+              {/* <Typography variant="caption">{msg}</Typography> */}
+              <Stack direction={"row"} alignItems="center" spacing={1}>
+                {incoming ? (
+                  <ArrowDownLeft color={missed ? "red" : "green"} />
+                ) : (
+                  <ArrowUpRight color={missed ? "red" : "green"} />
+                )}
+                <Typography variant="caption">
+                  Yesterday 21:24
+                </Typography>
+              </Stack>
+            </Stack>
           </Stack>
-       
-
-      </Stack>
-
-
+        </Stack>
       </Box>
     </>
   );
@@ -50,4 +61,4 @@ const CallLogElement = ({online}) => {
 
 const CallElement = () => {};
 
-export {CallElement, CallLogElement};
+export { CallElement, CallLogElement };
