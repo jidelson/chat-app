5898267883533be23bb75a4d7cc937f94ae607b4
Joe Idelson <joeidelson@gmail.com>
2024-09-08 16:46:59 -0500

    did initial layout for profile pg

---
+++ src/pages/dashboard/Profile.js
@@ -0,0 +1,26 @@
+import { Box, Stack } from '@mui/material';
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <>
+            <Stack direction={"row"} sx={{ width: "100%" }}>
+            <Box
+          sx={{
+            height: "100vh",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+            width: 320,
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+            
+            </Box> 
+            </Stack>
+        </>
+    );
+};
+
+export default Profile;
--- src/routes/index.js
+++ src/routes/index.js
@@ -39,6 +39,7 @@ export default function Router() {
         { path: "settings", element: <Settings /> },
         { path: "group", element: <GroupPage /> },
         { path: "call", element: <CallPage /> },
+        {path: "profile", element: <ProfilePage />},
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
@@ -62,3 +63,4 @@ const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
 const GroupPage = Loadable(lazy(() => import("../pages/dashboard/Group")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
+const ProfilePage = Loadable(lazy(() => import("../pages/dashboard/Profile")));
