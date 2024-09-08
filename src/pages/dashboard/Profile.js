6ed81947ecb950abae5ce5154faa3e14ddfecfbb
Joe Idelson <joeidelson@gmail.com>
2024-09-08 17:04:24 -0500

    added profile form

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,12 +1,13 @@
-import { Box, IconButton, Stack, Typography } from '@mui/material';
-import { CaretLeft } from 'phosphor-react';
-import React from 'react';
+import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { CaretLeft } from "phosphor-react";
+import React from "react";
+import ProfileForm from "../../sections/settings/ProfileForm";
 
 const Profile = () => {
-    return (
-        <>
-            <Stack direction={"row"} sx={{ width: "100%" }}>
-            <Box
+  return (
+    <>
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        <Box
           sx={{
             height: "100vh",
             backgroundColor: (theme) =>
@@ -17,25 +18,24 @@ const Profile = () => {
             boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
           }}
         >
-            <Stack p={4} spacing={5}>
-                {/* Header */}
-                <Stack direction="row" alignItems={"center"} spacing={3}>
-                    <IconButton>
-                        <CaretLeft size={24} color={"#4B4B4B"} />
-
-                    </IconButton>
-                    <Typography variant='h5'>
-                        Profile
-                    </Typography>
-
-                </Stack>
-
+          <Stack p={4} spacing={5}>
+            {/* Header */}
+            <Stack direction="row" alignItems={"center"} spacing={3}>
+              <IconButton>
+                <CaretLeft size={24} color={"#4B4B4B"} />
+              </IconButton>
+              <Typography variant="h5">Profile</Typography>
             </Stack>
 
-            </Box> 
-            </Stack>
-        </>
-    );
+            {/* Profile Form */}
+            <ProfileForm />
+
+
+          </Stack>
+        </Box>
+      </Stack>
+    </>
+  );
 };
 
 export default Profile;
