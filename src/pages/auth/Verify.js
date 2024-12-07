b21097fc920a01dd913f77a597e00ce09c7e1e6f
Joe Idelson <joeidelson@gmail.com>
2024-12-07 13:26:34 -0600

    styled some in verify.js

--- src/pages/auth/Verify.js
+++ src/pages/auth/Verify.js
@@ -5,9 +5,9 @@ const Verify = () => {
   return (
     <>
       <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
-        <Typography>Please Verify OTP</Typography>
-        <Stack>
-          <Typography>Sent to email (joeidelson@gmail.com)</Typography>
+        <Typography variant="h4">Please Verify OTP</Typography>
+        <Stack direction={"row"} spacing={0.5}>
+          <Typography variant="body2">Sent to email (joeidelson@gmail.com)</Typography>
         </Stack>
       </Stack>
     </>
